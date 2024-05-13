4aa8b1136e157f8d263c667bf63e6b0a8ff60b38
Xaidev <neclxas3311@gmail.com>
2024-05-12 21:51:29 -0400

    Arreglo Subclase Retraso debido al cambio en Asistencia

--- src/main/java/org/example/Retraso.java
+++ src/main/java/org/example/Retraso.java
@@ -7,21 +7,20 @@ public class Retraso extends Asistencia {
 
     private final ArrayList<Empleado> listaDeRetrasos;
 
-    public Retraso(ArrayList<Empleado> listaEmpleados, Instant horaPrevista) {
+    public Retraso(ArrayList<Empleado> listaEmpleados, Instant horaPrevista){
         super(listaEmpleados, horaPrevista);
         listaDeRetrasos = new ArrayList<Empleado>();
     }
 
     /**
-     * Método para ver que empleado ha llegado tarde
-     *
+     *  Método para ver que empleado ha llegado tarde
      * @return Lista de empleados que se retrasaron en la reunión
      */
-    public ArrayList getObtenerRetrasos() {
-        for (int i = 0; i < super.listaEmpleados.size(); i++) {
-            if (super.listaHoraDeLlegada.get(i).isAfter(super.getHoraDeLlegada())) {
-                super.listaDeAsistencias.add(super.listaInvitados.get(i));
-                listaDeRetrasos.add(super.listaEmpleados.get(i));
+    public ArrayList<Empleado> getObtenerRetrasos() {
+        for (int i = 0; i < super.getListaInvitados().size(); i++) {
+            if (super.getListaHoraDeLlegada().get(i).isAfter(super.getHoraPrevista())) {
+                super.getListaDeAsistencias().add(super.getListaInvitados().get(i));
+                listaDeRetrasos.add(super.getListaInvitados().get(i));
             }
         }
         return listaDeRetrasos;
