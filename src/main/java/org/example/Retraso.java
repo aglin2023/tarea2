872fa738a9ae9a4ee550d7d2adf07992354cda9e
Xaidev <neclxas3311@gmail.com>
2024-05-12 21:36:28 -0400

    fix clase Retraso

--- src/main/java/org/example/Retraso.java
+++ src/main/java/org/example/Retraso.java
@@ -5,24 +5,29 @@ import java.util.ArrayList;
 
 public class Retraso extends Asistencia {
 
-    ArrayList<Empleado> listaDeRetrasos;
+    private final ArrayList<Empleado> listaDeRetrasos;
 
-    public Retraso(ArrayList<Empleado> listaEmpleados, Instant horaPrevista){
+    public Retraso(ArrayList<Empleado> listaEmpleados, Instant horaPrevista) {
         super(listaEmpleados, horaPrevista);
         listaDeRetrasos = new ArrayList<Empleado>();
     }
 
     /**
-     *  Método para ver que empleado ha llegado tarde
+     * Método para ver que empleado ha llegado tarde
+     *
      * @return Lista de empleados que se retrasaron en la reunión
      */
-    public ArrayList getObtenerRetrasos(){
-        for(int i = 0; i < super.listaEmpleados.size(); i++) {
-            if(super.listaHoraDeLlegada.get(i).isAfter(super.horaDeLlegada)){
+    public ArrayList getObtenerRetrasos() {
+        for (int i = 0; i < super.listaEmpleados.size(); i++) {
+            if (super.listaHoraDeLlegada.get(i).isAfter(super.getHoraDeLlegada())) {
+                super.listaDeAsistencias.add(super.listaInvitados.get(i));
                 listaDeRetrasos.add(super.listaEmpleados.get(i));
             }
         }
         return listaDeRetrasos;
     }
 
+    public ArrayList<Empleado> getListaDeRetrasos() {
+        return listaDeRetrasos;
+    }
 }
