c1f1d0c86157743e85d2331a1c8b6bc9997bd701
Xaidev <neclxas3311@gmail.com>
2024-05-12 21:37:56 -0400

    fix Clase Reunion agrego getters y setters

--- src/main/java/org/example/Reunion.java
+++ src/main/java/org/example/Reunion.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.time.Instant;
 import java.time.Duration;
 import java.util.Date;
@@ -17,7 +18,7 @@ public abstract class Reunion {
     private Retraso retraso;
 
 
-    public Reunion(Date fecha, Duration duracionPrevista, Instant horaPrevista){
+    public Reunion(Date fecha, Duration duracionPrevista, Instant horaPrevista) {
         this.fecha = fecha;
         this.duracionPrevista = duracionPrevista;
         this.horaPrevista = horaPrevista;
@@ -27,33 +28,41 @@ public abstract class Reunion {
 
     }
 
-    public ArrayList obtenerAsistencias(){
+    public ArrayList obtenerAsistencias() {
         return asistencia.getObtenerAsistencia();
     }
-    public ArrayList obtenerAusencias(){
+
+    public ArrayList obtenerAusencias() {
         return asistencia.getListaDeAusencias();
     }
-    public ArrayList obtenerRetrasos(){
+
+    public ArrayList obtenerRetrasos() {
         return retraso.getListaDeRetrasos();
     }
-    public int obtenerTotalAsistencias(){
+
+    public int obtenerTotalAsistencias() {
         return asistencia.getListaDeAsistencias().size();
     }
-    public float obtenerPorcentajeAsistencia(){
-        return (float)obtenerTotalAsistencias() * 100 / (float)listaInvitable.size();
+
+    public float obtenerPorcentajeAsistencia() {
+        return (float) obtenerTotalAsistencias() * 100 / (float) listaInvitable.size();
     }
-    public float calcularTiempoReal(Instant horaInicio, Instant horaFin){
+
+    public float calcularTiempoReal(Instant horaInicio, Instant horaFin) {
         Duration tiempoReal = Duration.between(horaInicio, horaFin);
-        return (float)tiempoReal.toMinutes();
+        return (float) tiempoReal.toMinutes();
     }
-    public void iniciar(){
+
+    public void iniciar() {
         this.horaInicio = Instant.now();
     }
-    public void finalizar(){
+
+    public void finalizar() {
         this.horaFin = Instant.now();
     }
-    public ArrayList<Empleado> listaInvitable(ArrayList<Departamento> departamentos){
-        for(Departamento departamento : departamentos) {
+
+    public ArrayList<Empleado> listaInvitable(ArrayList<Departamento> departamentos) {
+        for (Departamento departamento : departamentos) {
             ArrayList<Empleado> listaEmpleados = departamento.ListaEmpleados();
             for (Empleado empleado : listaEmpleados) {
                 listaInvitable.add(empleado);
@@ -65,19 +74,24 @@ public abstract class Reunion {
     public ArrayList<Empleado> getListaInvitable() {
         return listaInvitable;
     }
-    public void setlistaInvitados(ArrayList<Empleado> listaInvitable){
+
+    public void setlistaInvitados(ArrayList<Empleado> listaInvitable) {
         listaInvitados.add(organizador.invitar(ListaInvitable));
     }
+
     public ArrayList<Empleado> getListaInvitados() {
         return listaInvitados;
     }
-    public void setAsistencia(){
+
+    public void setAsistencia() {
         this.asistencia = new Asistencia(listaInvitados, horaPrevista);
     }
+
     public void setRetraso() {
         this.retraso = new Retraso(listaInvitados, horaPrevista);
     }
-    public ArrayList<Empleado> getListaInvitados(){
+
+    public ArrayList<Empleado> getListaInvitados() {
         return listaInvitados;
     }
 }
