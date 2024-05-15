7f2fcb60fa62c46ab70e490bba5226a8d8ab77b8
Xaidev <neclxas3311@gmail.com>
2024-05-15 17:25:33 -0400

    Agrego metodo toString a la clase Invitacion

--- src/main/java/org/example/Invitacion.java
+++ src/main/java/org/example/Invitacion.java
@@ -5,13 +5,26 @@ import java.util.ArrayList;
 
 public class Invitacion {
     private Instant horaDeInvitacion;
-    private ArrayList<Invitable> listaInvitados;
+    private ArrayList<Empleado> listaInvitados;
 
     public Invitacion() {
-        this.listaInvitados = new ArrayList<Invitable>();
+        this.listaInvitados = new ArrayList<Empleado>();
+        this.horaDeInvitacion = Instant.now();
     }
 
-    public ArrayList<Invitable> getListaInvitados() {
+    public ArrayList<Empleado> getListaInvitados() {
         return listaInvitados;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hora de invitación: " + horaDeInvitacion.toString()).append("\n");
+        sb.append("Lista de invitados: ");
+        if(listaInvitados != null) {
+            for(Empleado empleado : listaInvitados) {
+                sb.append(empleado.toString()).append("\n");
+            }
+        }
+        return sb.toString();
     }
 }
