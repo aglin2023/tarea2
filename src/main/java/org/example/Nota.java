9af8d40e82a6ac6e12d920b00da99f5307cd5c79
Sebastian Garcias <s.garcias1963@gmail.com>
2024-05-15 00:19:51 -0400

    Nota y TipoReunion

---
+++ src/main/java/org/example/Nota.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.time.Instant;
+
+public class Nota {
+
+    private String contenido;
+    private Instant ultimoCambio;
+
+    public Nota(String s){
+	contenido = s;
+	ultimoCambio = Instant.now();
+    }
+
+    public String LeerNota(){
+	return contenido + " -- " + ultimoCambio;
+    }
+
+    // Cambia la nota
+    public void CambiarNota(String n){
+	contenido = n;
+	ultimoCambio = Instant.now();
+    }
+}
