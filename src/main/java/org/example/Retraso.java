9596ee115d8aae208d59d262ea0e89bdeb6dfde9
Xaidev <neclxas3311@gmail.com>
2024-05-15 17:27:32 -0400

    Creacion de metodo toString en clase Retraso

--- src/main/java/org/example/Retraso.java
+++ src/main/java/org/example/Retraso.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
 
 public class Retraso extends Asistencia {
 
-    private final ArrayList<Invitable> listaDeRetrasos;
+    private ArrayList<Empleado> listaDeRetrasos;
 
-    public Retraso(ArrayList<Invitable> listaInvitados, Instant horaPrevista){
+    public Retraso(ArrayList<Empleado> listaInvitados, Instant horaPrevista){
         super(listaInvitados, horaPrevista);
-        listaDeRetrasos = new ArrayList<Invitable>();
+        this.listaDeRetrasos = new ArrayList<>();
     }
 
     /**
      *  Método para ver que empleado ha llegado tarde
      * @return Lista de empleados que se retrasaron en la reunión
      */
-    public ArrayList<Invitable> getObtenerRetrasos() {
+    public ArrayList<Empleado> getObtenerRetrasos() {
         for (int i = 0; i < super.getListaInvitados().size(); i++) {
             if (super.getListaHoraDeLlegada().get(i).isAfter(super.getHoraPrevista())) {
                 super.getListaDeAsistencias().add(super.getListaInvitados().get(i));
@@ -26,7 +26,24 @@ public class Retraso extends Asistencia {
         return listaDeRetrasos;
     }
 
-    public ArrayList<Invitable> getListaDeRetrasos() {
+    public ArrayList<Empleado> getListaDeRetrasos() {
         return listaDeRetrasos;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lista de Retrasos: ");
+        if (listaDeRetrasos != null) {
+            int horaDeLlegadaEmpleado = 0;
+            for(Empleado empleado : listaDeRetrasos) {
+                sb.append(empleado.toString()).append(", ");
+                sb.append(super.getListaHoraDeLlegada().get(horaDeLlegadaEmpleado));
+                sb.append("\n");
+                horaDeLlegadaEmpleado++;
+            }
+        } else {
+            sb.append("No hubo retrasos\n");
+        }
+        return sb.toString();
     }
 }
