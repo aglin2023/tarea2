d3a5305d8bc88c7bcafee8ae92960aa69e2cde4f
Xaidev <neclxas3311@gmail.com>
2024-05-16 13:25:01 -0400

    Arreglo en import InformeTest

--- src/test/java/org/example/InformeTest.java
+++ src/test/java/org/example/InformeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
